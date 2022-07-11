b26937a3ac2e5055aa377a05a37d1af39f24ec32
Astrov003 <vpopovic003@gmail.com>
2022-07-11 21:21:42 +0200

    notes placed, look good -> added page.css from cs50 website.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect
 from flask_session import Session
 import sqlite3
 
-from helpers import login_required
+#from helpers import login_required
 
 app = Flask(__name__)
 
@@ -16,17 +16,32 @@ Session(app)
 conn = sqlite3.connect("plus50.db")
 db = conn.cursor()
 
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headres["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return responses
-
+video_url = [
+    "https://www.youtube.com/embed/4zy0z5W0-w4",
+    "https://www.youtube.com/embed/Na2wiHOnzXU",
+    "https://www.youtube.com/embed/xC3BZa1pcsY",
+    "https://www.youtube.com/embed/EXnHX-6voug",
+    "https://www.youtube.com/embed/l-dHFS_Pnzc",
+    "https://www.youtube.com/embed/urRlbmW5Txw",
+    "https://www.youtube.com/embed/k9RO-eEF9xo",
+    "https://www.youtube.com/embed/1YVIBFODn58",
+    "https://www.youtube.com/embed/lp-FwJwCqpE",
+    "https://www.youtube.com/embed/mlRlDFAyPtE",
+    "https://www.youtube.com/embed/oe-Iz0j1n6I"
+]
 
 @app.route("/")
-@login_required
+#@login_required
 def index():
-
     return render_template("index.html")
+
+@app.route("/video")
+#@login_required
+def video():
+    return render_template("video.html", video=video_url[1])
+
+@app.route("/notes")
+#@login_required
+def notes():
+    return render_template("notes.html")
     

e6bfcd96b7871d0db9bf0f6134f44a46d7266e86
Astrov003 <vpopovic003@gmail.com>
2022-07-16 14:57:19 +0200

    added lecture index. entire top row is in each lecture segment template

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import os
-
-from http.client import responses
 from flask import Flask, render_template, redirect
 
 
@@ -9,47 +7,41 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("video.html")
-
-"""
-@app.route(f"/lecture{lecture_index}")
-def lecture():
-    return redirect("video.html", lecture_index=lecture_index)
-"""
+    return redirect("lecture0")
 
 
-@app.route("/video")
-def video():
+@app.route("/lecture<index>")
+def lecture(index):
+    videos = [
+        "https://www.youtube.com/embed/4zy0z5W0-w4",
+        "https://www.youtube.com/embed/Na2wiHOnzXU",
+        "https://www.youtube.com/embed/xC3BZa1pcsY",
+        "https://www.youtube.com/embed/EXnHX-6voug",
+        "https://www.youtube.com/embed/l-dHFS_Pnzc",
+        "https://www.youtube.com/embed/urRlbmW5Txw",
+        "https://www.youtube.com/embed/k9RO-eEF9xo",
+        "https://www.youtube.com/embed/1YVIBFODn58",
+        "https://www.youtube.com/embed/lp-FwJwCqpE",
+        "https://www.youtube.com/embed/mlRlDFAyPtE",
+        "https://www.youtube.com/embed/oe-Iz0j1n6I"
+    ] 
     # Put it into database?
-    video_url = [
-    "https://www.youtube.com/embed/4zy0z5W0-w4",
-    "https://www.youtube.com/embed/Na2wiHOnzXU",
-    "https://www.youtube.com/embed/xC3BZa1pcsY",
-    "https://www.youtube.com/embed/EXnHX-6voug",
-    "https://www.youtube.com/embed/l-dHFS_Pnzc",
-    "https://www.youtube.com/embed/urRlbmW5Txw",
-    "https://www.youtube.com/embed/k9RO-eEF9xo",
-    "https://www.youtube.com/embed/1YVIBFODn58",
-    "https://www.youtube.com/embed/lp-FwJwCqpE",
-    "https://www.youtube.com/embed/mlRlDFAyPtE",
-    "https://www.youtube.com/embed/oe-Iz0j1n6I"
-    ]
-    return render_template("video.html", video=video_url[1])
+    return render_template("video.html", video=videos[int(index)], index=index)
 
 
-@app.route("/notes")
-def notes():
+@app.route("/notes<index>")
+def notes(index):
     notes = "files/notes/lecture1.html"
-    return render_template("notes.html", notes=notes)
+    return render_template("notes.html", notes=notes, index=index)
 
 
-@app.route("/slides")
-def slides():
-    return render_template("slides.html")
+@app.route("/slides<index>")
+def slides(index):
+    return render_template("slides.html", index=index)
 
 
-@app.route("/source")
-def source():
+@app.route("/source<index>")
+def source(index):
     # List to hold all source files per lecture
     source = []
 
@@ -60,17 +52,16 @@ def source():
     for f in os.listdir(path):
         with open(os.path.join(path, f)) as f:
             source.append(os.path.basename(f.name))
+    return render_template("source.html", source=source, index=index)
 
-    return render_template("source.html", source=source)
 
-
-@app.route("/transcript")
-def transcript():
+@app.route("/transcript<index>")
+def transcript(index):
     return render_template("transcript.html")
 
 
-@app.route("/shorts")
-def shorts():
+@app.route("/shorts<index>")
+def shorts(index):
     l1_shorts = [
         "https://www.youtube.com/embed/Fc9htmvVZ9U",
         "https://www.youtube.com/embed/f1xZf4iJDWE",
@@ -78,12 +69,14 @@ def shorts():
         "https://www.youtube.com/embed/WgX8e_O7eG8",
         "https://www.youtube.com/embed/BnJ013X02b8"
     ]
-    return render_template("shorts.html", shorts=l1_shorts)
+    return render_template("shorts.html", shorts=l1_shorts, index=index)
 
-@app.route("/pset0")
-def pset0():
-    return render_template("pset0.html")
 
-@app.route("/pset1")
-def pset1():
-    return render_template("pset1.html")
+@app.route("/problems<index>")
+def pset0(index):
+    return render_template("problems.html", index=index)
+
+
+@app.route("/pset1<index>")
+def pset1(index):
+    return render_template("pset1.html", index=index)
